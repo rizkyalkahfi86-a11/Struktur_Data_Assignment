9f31b52119cf1d8ca613412e64b6893c7389866e
rizkyalkahfi86-a11 <rizkyalkahfi86@gmail.com>
2025-11-20 10:09:13 +0700

    Update SLLInventory.h

--- MODUL-9 ASESMEN/soal-1/SLLInventory.h
+++ MODUL-9 ASESMEN/soal-1/SLLInventory.h
@@ -1,52 +1,49 @@
-// #ifndef SLLINVENTORY_H
-// #define SLLINVENTORY_H
+#ifndef SLLINVENTORY_H
+#define SLLINVENTORY_H
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// //  STRUCT PRODUCT
-// struct Product {
-//     string Nama;
-//     string SKU;
-//     int Jumlah;
-//     float HargaSatuan;
-//     float DiskonPersen; 
-// };
+struct Product {
+    string Nama;
+    string SKU;
+    int Jumlah;
+    float HargaSatuan;
+    float DiskonPersen; 
+};
 
-// typedef struct Node* address;
+typedef struct Node* address;
 
-// //  STRUCT NODE
-// struct Node {
-//     Product info;
-//     address next;
-// };
+struct Node {
+    Product info;
+    address next;
+};
 
-// //  STRUCT LIST
-// struct List {
-//     address head;
-// };
+struct List {
+    address head;
+};
 
-// //  DEKLARASI ADT
-// bool isEmpty(List L);
-// void createList(List &L);
-// address allocate(Product P);
-// void deallocate(address addr);
+bool isEmpty(List L);
+void createList(List &L);
+address allocate(Product P);
+void deallocate(address addr);
 
-// void insertFirst(List &L, Product P);
-// void insertLast(List &L, Product P);
-// void insertAfter(List &L, address Q, Product P);
+void insertFirst(List &L, Product P);
+void insertLast(List &L, Product P);
+void insertAfter(List &L, address Q, Product P);
 
-// void deleteFirst(List &L, Product &P);
-// void deleteLast(List &L, Product &P);
-// void deleteAfter(List &L, address Q, Product &P);
+void deleteFirst(List &L, Product &P);
+void deleteLast(List &L, Product &P);
+void deleteAfter(List &L, address Q, Product &P);
 
-// void updateAtPosition(List &L, int posisi);
-// void viewList(List L);
-// void searchByFinalPriceRange(List L, float minPrice, float maxPrice);
+void updateAtPosition(List &L, int posisi);
+void viewList(List L);
+void searchByFinalPriceRange(List L, float minPrice, float maxPrice);
 
-// // ==========================
-// //   BAGIAN B (DITAMBAHKAN)
-// // ==========================
-// void MaxHargaAkhir(List L);   // mencari produk dengan HargaAkhir terbesar
+// ==========================
+//   BAGIAN B (DITAMBAHKAN)
+// ==========================
+void MaxHargaAkhir(List L);  
 
-// #endif
+#endif
+    
