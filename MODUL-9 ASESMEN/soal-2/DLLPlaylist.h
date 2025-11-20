ec03954ba2f70814bf77d1b73cd9216b8cb7ae08
rizkyalkahfi86-a11 <rizkyalkahfi86@gmail.com>
2025-11-20 10:08:35 +0700

    Update DLLPlaylist.h

--- MODUL-9 ASESMEN/soal-2/DLLPlaylist.h
+++ MODUL-9 ASESMEN/soal-2/DLLPlaylist.h
@@ -1,50 +1,49 @@
-// #ifndef DLLPLAYLIST_H
-// #define DLLPLAYLIST_H
+#ifndef DLLPLAYLIST_H
+#define DLLPLAYLIST_H
 
-// #include <iostream>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <string>
+using namespace std;
 
-// struct Song {
-//     string Title;
-//     string Artist;
-//     int DurationSec;
-//     int PlayCount;
-//     float Rating;   // 0.0 - 5.0
-// };
+struct Song {
+    string Title;
+    string Artist;
+    int DurationSec;
+    int PlayCount;
+    float Rating;   
+};
 
-// struct Node {
-//     Song info;
-//     Node* prev;
-//     Node* next;
-// };
+struct Node {
+    Song info;
+    Node* prev;
+    Node* next;
+};
 
-// struct List {
-//     Node* head;
-//     Node* tail;
-// };
+struct List {
+    Node* head;
+    Node* tail;
+};
 
-// // ==== DEKLARASI FUNGSI / PROSEDUR ====
 
-// bool isEmpty(List L);
-// void createList(List &L);
-// Node* allocate(Song S);
-// void deallocate(Node* P);
+bool isEmpty(List L);
+void createList(List &L);
+Node* allocate(Song S);
+void deallocate(Node* P);
 
-// void insertFirst(List &L, Song S);
-// void insertLast(List &L, Song S);
-// void insertAfter(List &L, Node* Q, Song S);
-// void insertBefore(List &L, Node* Q, Song S);
+void insertFirst(List &L, Song S);
+void insertLast(List &L, Song S);
+void insertAfter(List &L, Node* Q, Song S);
+void insertBefore(List &L, Node* Q, Song S);
 
-// void deleteFirst(List &L, Song &S);
-// void deleteLast(List &L, Song &S);
-// void deleteAfter(List &L, Node* Q, Song &S);
-// void deleteBefore(List &L, Node* Q, Song &S);
+void deleteFirst(List &L, Song &S);
+void deleteLast(List &L, Song &S);
+void deleteAfter(List &L, Node* Q, Song &S);
+void deleteBefore(List &L, Node* Q, Song &S);
 
-// void updateAtPosition(List &L, int posisi);
-// void updateBefore(List &L, Node* Q);
+void updateAtPosition(List &L, int posisi);
+void updateBefore(List &L, Node* Q);
 
-// void viewList(List L);
-// void searchByPopularityRange(List L, float minScore, float maxScore);
+void viewList(List L);
+void searchByPopularityRange(List L, float minScore, float maxScore);
 
-// #endif
+#endif
