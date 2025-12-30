6ca98e143b0dc887fcdf071fb109bac38fa0f59d
rizkyalkahfi86-a11 <rizkyalkahfi86@gmail.com>
2025-12-30 17:19:31 +0700

    Add files via upload

---
+++ Modul 14/Unguided/soal 2/graph.cpp
@@ -0,0 +1,67 @@
+#include "graph.h"
+
+void CreateGraph(Graph &G) {
+    G.first = NULL;
+}
+
+adrNode InsertNode(Graph &G, infoGraph x) {
+    adrNode P = new ElmNode;
+    P->info = x;
+    P->visited = 0;
+    P->firstEdge = NULL;
+    P->next = NULL;
+
+    if (G.first == NULL) {
+        G.first = P;
+    } else {
+        adrNode Q = G.first;
+        while (Q->next != NULL) {
+            Q = Q->next;
+        }
+        Q->next = P;
+    }
+    return P;
+}
+
+void ConnectNode(adrNode N1, adrNode N2) {
+    adrEdge E1 = new ElmEdge;
+    E1->node = N2;
+    E1->next = N1->firstEdge;
+    N1->firstEdge = E1;
+
+    adrEdge E2 = new ElmEdge;
+    E2->node = N1;
+    E2->next = N2->firstEdge;
+    N2->firstEdge = E2;
+}
+
+void PrintInfoGraph(Graph G) {
+    cout << "ISI GRAPH (ADJACENCY LIST)" << endl;
+
+    adrNode P = G.first;
+    while (P != NULL) {
+        cout << "Node " << P->info;
+        adrEdge E = P->firstEdge;
+        while (E != NULL) {
+            cout << " -> " << E->node->info;
+            E = E->next;
+        }
+        cout << endl;
+        P = P->next;
+    }
+}
+
+void PrintDFS(Graph G, adrNode N) {
+    if (N == NULL) return;
+
+    N->visited = 1;
+    cout << N->info << " ";
+
+    adrEdge E = N->firstEdge;
+    while (E != NULL) {
+        if (E->node->visited == 0) {
+            PrintDFS(G, E->node);
+        }
+        E = E->next;
+    }
+}
