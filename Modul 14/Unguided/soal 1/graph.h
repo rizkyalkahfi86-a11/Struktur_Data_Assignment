0283f806579219da69e865fc18d5888d603a10a5
rizkyalkahfi86-a11 <rizkyalkahfi86@gmail.com>
2025-12-30 17:18:53 +0700

    Add files via upload

---
+++ Modul 14/Unguided/soal 1/graph.h
@@ -0,0 +1,32 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+using namespace std;
+
+typedef char infoGraph;
+typedef struct ElmNode *adrNode;
+typedef struct ElmEdge *adrEdge;
+
+struct ElmEdge {
+    adrNode node;
+    adrEdge next;
+};
+
+struct ElmNode {
+    infoGraph info;
+    int visited;
+    adrEdge firstEdge;
+    adrNode next;
+};
+
+struct Graph {
+    adrNode first;
+};
+
+void CreateGraph(Graph &G);
+adrNode InsertNode(Graph &G, infoGraph x);
+void ConnectNode(adrNode N1, adrNode N2);
+void PrintInfoGraph(Graph G);
+
+#endif
